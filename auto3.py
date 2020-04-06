b5e9ae24337248e42b074e8693fa8ccc2655fdd0
verysetiawan <kangphery@gmail.com>
2020-04-06 22:42:05 +0700

    menambahkan ket identity

--- auto3.py
+++ auto3.py
@@ -5,7 +5,9 @@ import sys
 
 try:
 #variabel ip (untuk ip yang diremote), username (untuk masuk ssh), password (password ssh)
-    ip_address = ["192.168.122.65","192.168.122.190","192.168.122.152","192.168.122.4"]
+    ip_address = ["192.168.122.100","192.168.122.223","192.168.122.140","192.168.122.127",
+                  "192.168.122.83","192.168.122.69","192.168.122.66","192.168.122.220"  
+                 ]
     username = "admin"
     password = ""
     i = 0
@@ -46,7 +48,7 @@ try:
         for command in config_list:
             ssh_client.exec_command(command)
             print (command)
-        print (f"Konfigurasi Router dengan identity : {i} dan IP Address : {ip} Berhasil")
+        print (f"Konfigurasi Router dengan identity : R{i} dan IP Address : {ip} Berhasil")
         time.sleep(0.5)
     sys.exit()
 
